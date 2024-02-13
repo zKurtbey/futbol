c20b7a57594334833995698705dbd8784c4a8bee
zKurtbey <140351544+zKurtbey@users.noreply.github.com>
2024-02-13 14:57:43 +0300

    asads

--- yonlendirme.js
+++
@@ -1,9 +0,0 @@
-if (window.location.pathname === "https://futbolunu.kanitla.online/anasayfa") {
-    window.location.replace("https://futbolunu.kanitla.online/index.html");
-}
-if (window.location.pathname === "https://futbolunu.kanitla.online/normalmod") {
-    window.location.replace("https://futbolunu.kanitla.online/normal.html");
-}
-if (window.location.pathname === "https://futbolunu.kanitla.online/golgelimod") {
-    window.location.replace("https://futbolunu.kanitla.online/golgeli.html");
-}
