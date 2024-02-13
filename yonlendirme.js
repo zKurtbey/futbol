754bc3e421b21878c9123f630853943d135e1b0c
zKurtbey <140351544+zKurtbey@users.noreply.github.com>
2024-02-13 14:45:29 +0300

    yonlendirme

---
+++ yonlendirme.js
@@ -0,0 +1,9 @@
+if (window.location.pathname === "/anasayfa") {
+    window.location.replace("/index.html");
+}
+if (window.location.pathname === "/normalmod") {
+    window.location.replace("/normal.html");
+}
+if (window.location.pathname === "/golgelimod") {
+    window.location.replace("/golgeli.html");
+}
